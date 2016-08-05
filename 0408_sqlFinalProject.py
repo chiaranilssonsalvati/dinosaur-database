03482da89349b937d2778ae843de30e90413870c
Chiara Nilsson-Salvati <chiaranilsson-salvati@BridgeUp-001.local>
2016-08-05 11:18:43 -0400

    add emojis to output

--- 0408_sqlFinalProject.py
+++ 0408_sqlFinalProject.py
@@ -57,6 +57,8 @@ def choice4():
 		else:
 			rowASCII.append(item)
 	print("The dinosaur "+rowASCII[0]+", whose name is pronounced "+rowASCII[1]+", means "+rowASCII[2]+".  The "+rowASCII[0]+" was named by "+rowASCII[3]+" in the year "+rowASCII[4]+".")
+	# s = u'\U0001f63b'
+	# print(s)
 	menu()
 
 
@@ -81,23 +83,33 @@ def errormessage():
 
 
 def welcome():
+	a = u'\U0001F432'
+	b = u'\U0001F409'
 	print("\nWelcome to the Dino Den!\n")
 	print(w.summary("Dinosaur", sentences=4))
 	print("\nOn this site, you will be able to learn about the lives of many different dinosaurs. \n")
+	print(a +"    " +b)
 	menu()
 
 def menu():
+	a = u'\U0001F30E'
+	b = u'\U0001F6B6'
+	c = u'\U0001F3C3'
+	d = u'\U0001F357'
+	e = u'\U0001F33F'
+	f = u'\U0001F4AC'
 	print('''You can choose to:
-	1. Learn about how a dinosaur's location relates to its diet.
-	2. Learn about how the period when a dinosaur lived related to how it moved.
-	3. Learn about how their diets related to their weights.
-	4. Learn about the etymology of a dinosaur's name.
-	''')
+	1. Learn about how a dinosaur's location relates to its diet. %s
+	2. Learn about how the period when a dinosaur lived related to how it moved. %s  %s
+	3. Learn about how their diets related to their weights. %s  %s
+	4. Learn about the etymology of a dinosaur's name. %s
+	''' % (a, b, c, d, e, f))
 	choice = raw_input("Choose 1, 2, 3, or 4, or 'quit' to quit. ").lower()
 	if choice != 'quit':
 		queries(choice)
 	else:
-		print("Thank you for visiting the Dino Den.  Goodbye!")
+		s = u'\U0001F44B'
+		print("Thank you for visiting the Dino Den.  Goodbye! "+s)
 
 
 
